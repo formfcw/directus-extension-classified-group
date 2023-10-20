fc6f8073f8814302fd429e0102741f46dc89121d
formfcw <dev@formfcw.com>
2023-10-20 09:01:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+# Classified Group Interface for Directus
+
+[![NPM version](https://img.shields.io/npm/v/directus-extension-classified-group)](https://www.npmjs.com/package/directus-extension-classified-group)
+
+A group to which a class can be assigned for custom styling.
+
+## Examples
+
+<details><summary>Example 1: Change spacing between fields</summary>
+
+1. Add a classified group
+   ![](docs/tight-interface.png)
+
+2. Add fields
+   ![](docs/tight-schema.png)
+
+3. Add Custom CSS in the Project Settings
+   ![](docs/tight-css.png)
+
+4. Result
+   ![](docs/tight-form.png)
+
+</details>
+
+<details><summary>Example 2: Add a border around fields</summary>
+
+1. Add a classified group
+   ![](docs/bordered-interface.png)
+
+2. Add fields
+   ![](docs/bordered-schema.png)
+
+3. Add Custom CSS in the Project Settings
+   ![](docs/bordered-css.png)
+
+4. Result
+   ![](docs/bordered-form.png)
+
+</details>
+
+<details><summary>Example 3: Place fields at the top of the directus_files collection</summary>
+
+1. Add a classified group
+   ![](docs/top-interface.png)
+
+2. Add fields
+   ![](docs/top-schema.png)
+
+3. Add Custom CSS in the Project Settings
+   ![](docs/top-css.png)
+
+4. Result
+   ![](docs/top-form.png)
+
+</details>
---
+++ src/interface.vue
@@ -0,0 +1,49 @@
+<template>
+    <div :class="classes">
+        <v-form
+            :initial-values="initialValues"
+            :fields="fields"
+            :model-value="values"
+            :primary-key="primaryKey"
+            :group="field.meta?.field"
+            :validation-errors="validationErrors"
+            :loading="loading"
+            :disabled="disabled"
+            :badge="badge"
+            :raw-editor-enabled="rawEditorEnabled"
+            :direction="direction"
+            :show-no-visible-fields="false"
+            :show-validation-errors="false"
+            @update:model-value="$emit('apply', $event)"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { Field, ValidationError } from '@directus/types';
+
+    withDefaults(
+        defineProps<{
+            classes: string;
+            field: Field;
+            fields: Field[];
+            values: Record<string, unknown>;
+            initialValues: Record<string, unknown>;
+            primaryKey: number | string;
+            disabled?: boolean;
+            batchMode?: boolean;
+            batchActiveFields?: string[];
+            loading?: boolean;
+            validationErrors?: ValidationError[];
+            badge?: string;
+            rawEditorEnabled?: boolean;
+            direction?: string;
+        }>(),
+        {
+            batchActiveFields: () => [],
+            validationErrors: () => [],
+        }
+    );
+
+    defineEmits(['apply']);
+</script>
